69109673cc879f296d06477648d79a3a7e6fd8db
Sebastian <sebasquinterop08@gmail.com>
2024-10-30 18:09:21 -0500

    Add files via upload

--- calculator_esp/main.py
+++ calculator_esp/main.py
@@ -1,5 +1,5 @@
 # Importar
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -15,8 +15,7 @@ def result_calculate(size, lights, device):
 @app.route('/')
 def index():
     return render_template('index.html')
-
-# La segunda página
+# Segunda página
 @app.route('/<size>')
 def lights(size):
     return render_template(
@@ -28,7 +27,7 @@ def lights(size):
 @app.route('/<size>/<lights>')
 def electronics(size, lights):
     return render_template(
-                            'electronics.html',
+                            'electronics.html',                           
                             size = size, 
                             lights = lights                           
                            )
@@ -42,4 +41,29 @@ def end(size, lights, device):
                                                     int(device)
                                                     )
                         )
+# El formulario
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+#Resultados del formulario
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    # Declarar variables para la recogida de datos
+    name = request.form['name']
+    email = request.form['email']
+    address = request.form['address']
+    date = request.form['date']
+
+    # Guardar los datos en un archivo .txt
+    with open('form.txt', 'a') as f:
+        f.write(f'Nombre: {name}\n')
+        f.write(f'E-Mail: {email}\n')
+        f.write(f'Dirección: {address}\n')
+        f.write(f'Fecha: {date}\n')
+        f.write('-------------------------\n')  # Separador para cada entrada
+
+    # Renderizar la plantilla con los datos
+    return render_template('form_result.html', name=name, email=email, address=address, date=date)
+
 app.run(debug=True)
